dfdc90d983fd1e0ed8b655003ecda3c858a05c3d
elezersolomon <elezersolomon2@gmail.com>
2021-09-01 02:25:32 +0300

    css collision

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
-
+import Header from './components/header.js';
 export default () => {
   return (
     <div>
-      <h1>Hi there!        </h1>
+      <BrowserRouter>
+      <h1>Hi there!     </h1>
       <hr />
+      <Header />
       <MarketingApp />
+      </BrowserRouter>
     </div>
   );
 };

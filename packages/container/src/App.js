3bca1c9d13edc504a6b34af8353a1a32fdf2fe16
elezersolomon <elezersolomon2@gmail.com>
2021-09-01 11:41:50 +0300

    css collision

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import { StylesProvider,createGenerateClassName } from '@material-ui/core/styles';
 import Header from './components/Header';
+
+const generateclassname = createGenerateClassName({
+  productionPrefix = 'co'
+})
 export default () => {
   return (
     <div>
       <BrowserRouter>
-      <h1>Hi there!     </h1>
+      <StylesProvider class = {generateclassname}>
+      <h1></h1>
       <hr />
       <Header />
       <MarketingApp />
+      </StylesProvider>
       </BrowserRouter>
+      
     </div>
   );
 };
